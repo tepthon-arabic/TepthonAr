db57cb3d22021653176c8421f1c4ddc1aa402926
Tepthonee <115221367+Tepthonee@users.noreply.github.com>
2023-01-13 12:17:02 +0300

    Update الاوامر.py

--- plugins/الاوامر.py
+++ plugins/الاوامر.py
@@ -1,10 +1,9 @@
 from plugins import ultroid_cmd
+from telethon import functions, types,events
+
+HMD = """[ . 𝐓𝐄𝐏𝐇𝐎𝐍 - 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒 ♛ .](t.me/Tepthone)\n 𓅔━ ━━━━✓━ 𓅔\n\n - آهـلاً وسـهلاً بك فـي قائـمة أوامـر السـورس [Click here|اضغـط هنـا](https://graph.org/%D8%A7%D9%88%D8%A7%D9%85%D8%B1-%D8%B3%D9%88%D8%B1%D8%B3-%D8%AA%D9%8A%D8%A8%D8%AB%D9%88%D9%86-%D8%A7%D9%84%D8%B9%D8%B1%D8%A8%D9%8A-01-11)\n\n"""
 
 
 @ultroid_cmd(pattern="الاوامر")
 async def hi(event):
-    await edit_or_reply(
-        eventually, 
-        """[ . 𝐑𝐄𝐏𝐓𝐇𝐎𝐍𓌶 - 𝐂𝐌𝐃- .](t.me/Repthon)\n 𓅔 ━━━━━ ━━—×—━━ ━━━━━ 𓅔\n\n - آهـلاً وسـهلاً بك فـي قائـمة أوامـر السـورس [Click here|اضغـط هنـا](https://graph.org/قائمة-اوامر-ريبثون-01-10)\n\n"""
-        link_preview=False,
-    )
+    await event.reply(f"{HMD}")
