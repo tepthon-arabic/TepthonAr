a849ad01790389fcae4ee35f50f8ac3d003c9196
Hmd Mostafa <98346135+xxe5@users.noreply.github.com>
2023-02-07 15:44:49 +0300

    Update اشتراك اجباري.py

--- plugins/اشتراك اجباري.py
+++ plugins/اشتراك اجباري.py
@@ -47,7 +47,7 @@ async def fsub(event):
                 return await event.reply("هذه القناة غير صالحة .")
         except:
             return await event.reply("يجب وضع المعرف بشكل صحيح.")
-        if not await participant_check(channel, ultroid_cmd.uid):
+        if not await participant_check(channel, ultroid_cmd.function):
             return await event.reply(
                 f"❗**أنا لست ادمن في هذه القناة**\n [القناة](https://t.me/{channel}). يجب ان اكون مشرف في القناة اولا.",
                 link_preview=False,
